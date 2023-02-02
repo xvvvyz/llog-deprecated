ef18507f6d24624bdd7b90c1999d1378d5cc9d7e
Cade Scroggins <git@cade.me>
2023-02-01 20:47:14 -0800

    adjust breadcrumb padding

--- app/(components)/breadcrumbs.tsx
+++ app/(components)/breadcrumbs.tsx
@@ -11,13 +11,13 @@ const Breadcrumbs = ({ items }: BreadcrumbProps) => {
   const pathname = usePathname();
 
   return (
-    <h1 className="ml-6 text-right">
+    <h1 className="ml-4 text-right">
       {items.length === 1 ? (
         <span className="text-2xl">{items[0]}</span>
       ) : (
         items.map(([label, href], i) => (
           <Button
-            className="after:pl-[0.2rem] after:pr-[0.6rem] after:text-fg-2 after:content-['/'] last:after:hidden last-of-type:text-fg-1"
+            className="after:pl-[0.4rem] after:pr-[0.75rem] after:text-fg-3 after:content-['/'] last:after:hidden last-of-type:text-fg-1"
             disabled={!href && i < items.length - 1}
             href={href ?? pathname ?? undefined}
             key={label}
