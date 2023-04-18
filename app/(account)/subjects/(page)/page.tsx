3215460c95dae92785e3f151d96ea9c37c006d9f
cade <git@cade.me>
2023-04-18 14:06:11 -0700

    separate client subjects from team subjects

--- app/(account)/subjects/(page)/page.tsx
+++ app/(account)/subjects/(page)/page.tsx
@@ -1,31 +1,51 @@
 import Empty from '(components)/empty';
 import LinkList from '(components)/link-list';
 import getCurrentTeamId from '(utilities)/get-current-team-id';
-import listSubjects from '(utilities)/list-subjects';
+import listSubjects, { ListSubjectsData } from '(utilities)/list-subjects';
 
 const Page = async () => {
   const { data: subjects } = await listSubjects();
   const currentTeamId = await getCurrentTeamId();
   if (!subjects?.length) return <Empty>Add a subject to get started</Empty>;
 
+  const { clientSubjects, teamSubjects } = subjects.reduce(
+    (acc, subject) => {
+      if (subject.team_id === currentTeamId) acc.teamSubjects.push(subject);
+      else acc.clientSubjects.push(subject);
+      return acc;
+    },
+    {
+      clientSubjects: [] as NonNullable<ListSubjectsData>,
+      teamSubjects: [] as NonNullable<ListSubjectsData>,
+    }
+  );
+
   return (
-    <LinkList>
-      {subjects.map((subject) => (
-        <LinkList.Item
-          avatar={subject.image_uri}
-          href={`/subjects/${subject.id}`}
-          key={subject.id}
-          text={subject.name}
-          {...(subject.team_id === currentTeamId
-            ? {
-                rightHref: `/subjects/${subject.id}/settings?back=/subjects`,
-                rightIcon: 'edit',
-                rightLabel: 'Edit',
-              }
-            : {})}
-        />
-      ))}
-    </LinkList>
+    <div className="space-y-4">
+      <LinkList>
+        {teamSubjects.map((subject) => (
+          <LinkList.Item
+            avatar={subject.image_uri}
+            href={`/subjects/${subject.id}`}
+            key={subject.id}
+            rightHref={`/subjects/${subject.id}/settings?back=/subjects`}
+            rightIcon="edit"
+            rightLabel="Edit"
+            text={subject.name}
+          />
+        ))}
+      </LinkList>
+      <LinkList>
+        {clientSubjects.map((subject) => (
+          <LinkList.Item
+            avatar={subject.image_uri}
+            href={`/subjects/${subject.id}`}
+            key={subject.id}
+            text={subject.name}
+          />
+        ))}
+      </LinkList>
+    </div>
   );
 };
 
