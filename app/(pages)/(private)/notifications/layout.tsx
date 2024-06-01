fab929369af38bb8cf571f865907286d0d04786c
cade <git@cade.me>
2024-06-01 14:06:40 -0700

    increase active tab contrast

--- app/(pages)/(private)/notifications/layout.tsx
+++ app/(pages)/(private)/notifications/layout.tsx
@@ -9,9 +9,9 @@ const Layout = ({ children }: PageProps) => (
   <div className="px-4">
     <div className="my-16 flex h-8 items-center justify-between gap-8">
       <h1 className="text-2xl">Notifications</h1>
-      <div className="flex divide-x divide-alpha-3 rounded-sm border border-alpha-3">
+      <div className="flex divide-x divide-alpha-2 rounded-sm border border-alpha-3">
         <Button
-          activeClassName="text-fg-2 bg-alpha-1"
+          activeClassName="text-fg-2 bg-alpha-2"
           colorScheme="transparent"
           className="rounded-r-none border-0"
           href="/notifications/inbox"
@@ -22,7 +22,7 @@ const Layout = ({ children }: PageProps) => (
           Inbox
         </Button>
         <Button
-          activeClassName="text-fg-2 bg-alpha-1"
+          activeClassName="text-fg-2 bg-alpha-2"
           colorScheme="transparent"
           className="rounded-l-none border-0"
           href="/notifications/archive"
