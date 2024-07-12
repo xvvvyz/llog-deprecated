88bb6cb3f5569e2f0fc5368c858ebc25281a80b8
cade <git@cade.me>
2024-07-12 12:25:28 -0600

    update sessions title length restriction to 100

--- supabase/migrations/20240709193309_add-sessions-title-length-constraint.sql
+++ supabase/migrations/20240709193309_add-sessions-title-length-constraint.sql
@@ -1,3 +1,3 @@
 update public.sessions set title = null where title = '';
-alter table "public"."sessions" add constraint "sessions_title_length" check (((length(title) > 0) and (length(title) < 50))) not valid;
+alter table "public"."sessions" add constraint "sessions_title_length" check (((length(title) > 0) and (length(title) < 100))) not valid;
 alter table "public"."sessions" validate constraint "sessions_title_length";
