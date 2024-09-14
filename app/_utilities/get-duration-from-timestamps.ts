324a365d916d02829b4c63df298c2cbce45d097b
cade <git@cade.me>
2024-09-14 17:48:06 -0600

    add specificity to getDurationFromTimestamp function

--- app/_utilities/get-duration-from-timestamps.ts
+++ app/_utilities/get-duration-from-timestamps.ts
@@ -2,7 +2,7 @@ import humanizeDurationShort from '@/_utilities/humanize-duration-short';
 
 const getDurationFromTimestamps = (start: string, end: string) =>
   humanizeDurationShort(new Date(end).getTime() - new Date(start).getTime(), {
-    largest: 1,
+    largest: 2,
   });
 
 export default getDurationFromTimestamps;
