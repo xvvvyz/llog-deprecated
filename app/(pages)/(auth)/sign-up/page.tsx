806645b857257a9ffaf86e899f46a50243855446
cade <git@cade.me>
2024-10-30 16:49:25 -0700

    prevent randoms from signing up in the preview env

--- app/(pages)/(auth)/sign-up/page.tsx
+++ app/(pages)/(auth)/sign-up/page.tsx
@@ -1,14 +1,21 @@
 import Button from '@/_components/button';
 import SignUpForm from '@/_components/sign-up-form';
+import { headers } from 'next/headers';
+import { redirect } from 'next/navigation';
 
 export const metadata = { title: 'Sign up' };
 
 interface PageProps {
-  searchParams: Promise<{ next?: string }>;
+  searchParams: Promise<{ next?: string; test?: string }>;
 }
 
 const Page = async ({ searchParams }: PageProps) => {
-  const { next } = await searchParams;
+  const { next, test } = await searchParams;
+
+  // hack to keep randoms from signing up in the preview env
+  if (test !== 'yes' && (await headers()).get('host') !== 'llog.app') {
+    redirect('https://llog.app/sign-up');
+  }
 
   return (
     <>
