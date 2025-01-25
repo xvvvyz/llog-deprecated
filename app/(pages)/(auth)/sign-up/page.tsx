86ac84bd585a29d74b0cc7f92ddc149213bea2a0
cade <git@cade.me>
2025-01-24 16:07:55 -0800

    remove preview env

--- app/(pages)/(auth)/sign-up/page.tsx
+++ app/(pages)/(auth)/sign-up/page.tsx
@@ -1,21 +1,14 @@
 import Button from '@/_components/button';
 import SignUpForm from '@/_components/sign-up-form';
-import { headers } from 'next/headers';
-import { redirect } from 'next/navigation';
 
 export const metadata = { title: 'Sign up' };
 
 interface PageProps {
-  searchParams: Promise<{ next?: string; test?: string }>;
+  searchParams: Promise<{ next?: string }>;
 }
 
 const Page = async ({ searchParams }: PageProps) => {
-  const { next, test } = await searchParams;
-
-  // hack to keep randoms from signing up in the preview env
-  if (test !== 'yes' && (await headers()).get('host') !== 'llog.app') {
-    redirect('https://llog.app/sign-up');
-  }
+  const { next } = await searchParams;
 
   return (
     <>
--- next.config.js
+++ next.config.js
@@ -9,13 +9,6 @@ const nextConfig = {
         hostname: 'localhost',
       },
       {
-        // preview
-        hostname: 'dpptopbnpbbqamunlkrd.supabase.co',
-        pathname: '/storage/v1/object/public/**',
-        protocol: 'https',
-      },
-      {
-        // production
         hostname: 'cnawksbzwuxsjkzeczyq.supabase.co',
         pathname: '/storage/v1/object/public/**',
         protocol: 'https',
